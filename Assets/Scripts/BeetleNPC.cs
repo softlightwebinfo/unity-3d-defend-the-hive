5649bc066d344adf015f4ff4b97cd4e2e0c91973
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-04-06 14:23:18 +0200

    Add new motor player

---
+++ Assets/Scripts/BeetleNPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeetleNPC : MonoBehaviour
+{
+    Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.m_Animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.m_Animator.Play("Die_OnGround");
+        }
+    }
+}
---
+++ Assets/Scripts/NewPlayerMotor.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Animator))]
+public class NewPlayerMotor : MonoBehaviour
+{
+
+
+    [SerializeField] float m_MovingTurnSpeed = 360;
+    [SerializeField] float m_StationaryTurnSpeed = 180;
+    [SerializeField] float m_JumpPower = 12f;
+    [SerializeField] float m_WalkSpeed = 10f;
+    [SerializeField] float m_RunSpeed = 20f;
+    [Range(1f, 10f)] [SerializeField] float m_GravityMultiplier = 2f;
+    [SerializeField] float m_MoveSpeedMultiplier = 1f;
+    [SerializeField] float m_AnimSpeedMultiplier = 1f;
+    [SerializeField] float m_GroundCheckDistance = 0.1f;
+
+    float currentSpeed;
+    Rigidbody m_Rigidbody;
+    Animator m_Animator;
+    bool m_IsGrounded;
+    float m_OrigGroundCheckDistance;
+    float m_TurnAmount;
+    float m_ForwardAmount;
+    Vector3 m_GroundNormal;
+
+    private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+    private Vector3 m_CamForward;             // The current forward direction of the camera
+    private Vector3 m_Move;
+
+
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        m_OrigGroundCheckDistance = m_GroundCheckDistance;
+
+        currentSpeed = m_WalkSpeed;
+        // get the transform of the main camera
+        if (Camera.main != null)
+        {
+            m_Cam = Camera.main.transform;
+        }
+        else
+        {
+            Debug.LogWarning(
+                "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+            // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+        }
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && m_IsGrounded)
+        {
+            TryToJump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && m_IsGrounded)
+        {
+            currentSpeed = m_RunSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            currentSpeed = m_WalkSpeed;
+        }
+    }
+
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        // read inputs
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        // calculate move direction to pass to character
+        if (m_Cam != null)
+        {
+            // calculate camera relative direction to move:
+            m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+            m_Move = v * m_CamForward + h * m_Cam.right;
+        }
+        else
+        {
+            // we use world-relative directions in the case of no main camera
+            m_Move = v * Vector3.forward + h * Vector3.right;
+        }
+
+
+        // pass all parameters to the character control script
+        /*
+        if (PlayerManager.hasDead == false)
+        {
+            Move(m_Move);
+        }
+        */
+    }
+
+    public void Move(Vector3 move)
+    {
+
+        // convert the world relative moveInput vector into a local-relative
+        // turn amount and forward amount required to head in the desired
+        // direction.
+        if (move.magnitude > 1f) move.Normalize();
+        move = transform.InverseTransformDirection(move);
+        CheckGroundStatus();
+        move = Vector3.ProjectOnPlane(move, m_GroundNormal);
+        m_TurnAmount = Mathf.Atan2(move.x, move.z);
+        m_ForwardAmount = move.z;
+
+        m_Rigidbody.velocity = transform.forward * m_ForwardAmount * currentSpeed;
+
+        ApplyExtraTurnRotation();
+
+        // control and velocity handling is different when grounded and airborne:
+        if (!m_IsGrounded)
+        {
+            HandleAirborneMovement();
+        }
+
+        // send input and other state parameters to the animator
+        UpdateAnimator(move);
+    }
+
+
+    void UpdateAnimator(Vector3 move)
+    {
+
+        // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
+        // which affects the movement speed because of the root motion.
+        if (m_IsGrounded && move.magnitude > 0)
+        {
+            m_Animator.speed = m_AnimSpeedMultiplier;
+        }
+        else
+        {
+            // don't use that while airborne
+            m_Animator.speed = 1;
+        }
+    }
+
+
+    void HandleAirborneMovement()
+    {
+        // apply extra gravity from multiplier:
+        Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
+        m_Rigidbody.AddForce(extraGravityForce);
+
+        m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
+    }
+
+
+    void TryToJump()
+    {
+
+        // jump!
+        m_Rigidbody.AddForce(0, m_JumpPower, 0);
+
+        //m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
+        m_IsGrounded = false;
+        m_Animator.applyRootMotion = false;
+        m_GroundCheckDistance = 0.1f;
+
+    }
+
+    void ApplyExtraTurnRotation()
+    {
+        // help the character turn faster (this is in addition to root rotation in the animation)
+        float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+        transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+    }
+
+
+    public void OnAnimatorMove()
+    {
+        // we implement this function to override the default root motion.
+        // this allows us to modify the positional speed before it's applied.
+        if (m_IsGrounded && Time.deltaTime > 0)
+        {
+            Vector3 v = (m_Animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
+
+            // we preserve the existing y part of the current velocity.
+            v.y = m_Rigidbody.velocity.y;
+            m_Rigidbody.velocity = v;
+        }
+    }
+
+
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // helper to visualise the ground check ray in the scene view
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
+#endif
+        // 0.1f is a small offset to start the ray from inside the character
+        // it is also good to note that the transform position in the sample assets is at the base of the character
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
+        {
+            m_GroundNormal = hitInfo.normal;
+            m_IsGrounded = true;
+            m_Animator.applyRootMotion = true;
+        }
+        else
+        {
+            m_IsGrounded = false;
+            m_GroundNormal = Vector3.up;
+            m_Animator.applyRootMotion = false;
+        }
+    }
+
+}
